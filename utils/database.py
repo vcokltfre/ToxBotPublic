2149f90c6c1a41d1ce25e4df68771093c922eb8a
vcokltfre <vcokltfre@gmail.com>
2021-02-14 18:51:34 +0000

    feat: add request limits to db

--- utils/database.py
+++ utils/database.py
@@ -1,6 +1,7 @@
 from asyncpg import create_pool
 from asyncio import get_event_loop
 from json import dumps, loads
+from time import time
 
 with open("./static/init.sql") as f:
     initscript = f.read()
@@ -20,6 +21,7 @@ class DatabaseInterface:
         get_event_loop().create_task(self.init())
 
         self.confcache = {}
+        self.guildreqs = {}
 
     async def hardreset(self):
         async with self.pool.acquire() as conn:
@@ -36,10 +38,12 @@ class DatabaseInterface:
         async with self.pool.acquire() as conn:
             await conn.execute(initscript)
 
+        await self.get_all_guild_requests()
+
     async def create_guild(self, guild: int, config: dict = None):
         try:
             async with self.pool.acquire() as conn:
-                await conn.execute("INSERT INTO Guilds (id, config) VALUES ($1, $2);", guild, dumps(config or self.default))
+                await conn.execute("INSERT INTO Guilds (id, config, lastreset) VALUES ($1, $2, $3);", guild, dumps(config or self.default), round(time()))
             return True
         except Exception as e:
             print(e)
@@ -77,3 +81,27 @@ class DatabaseInterface:
             await conn.execute("UPDATE Guilds SET config = $1 WHERE id = $2", dumps(self.default), guild)
 
         self.confcache[guild] = self.deafult
+
+    async def get_all_guild_requests(self):
+        async with self.pool.acquire() as conn:
+            results = await conn.fetch("SELECT (id, daily, today, lastreset) FROM Guilds;")
+
+        for result in results:
+            result = result[0]
+            self.guildreqs[result[0]] = {
+                "limit": result[1],
+                "today": result[2],
+                "reset": result[3]
+            }
+
+    async def reset_requests(self):
+        async with self.pool.acquire() as conn:
+            await conn.execute("UPDATE Guilds SET today = 0, lastreset = $1 WHERE lastreset < $1;", round(time() - 86400))
+
+        await self.get_all_guild_requests()
+
+    async def add_request(self, guild: int):
+        async with self.pool.acquire() as conn:
+            await conn.execute("UPDATE Guilds SET today = today + 1 WHERE id = $1;", guild)
+
+        self.guildreqs[guild]["today"] += 1
