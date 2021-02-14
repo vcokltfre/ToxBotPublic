8dfb561c4548e180f98dd9de33a401564d0b5b7c
vcokltfre <vcokltfre@gmail.com>
2021-02-14 16:42:37 +0000

    feat: add noadmin check

---
+++ utils/checks.py
@@ -0,0 +1,11 @@
+from discord.ext.commands import check, Context
+
+def noadmin():
+    async def predicate(ctx: Context):
+        if ctx.me.guild_permissions.administrator:
+            print(ctx.me.guild_permissions)
+            await ctx.reply("I can't execute commands while I have the administrator permission.")
+            return False
+        return True
+
+    return check(predicate)
