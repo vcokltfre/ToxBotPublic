f7883e296f52d6558bbe59a901c819cdd8321e24
vcokltfre <vcokltfre@gmail.com>
2021-02-14 17:57:58 +0000

    feat: no admin check in dev env

--- utils/checks.py
+++ utils/checks.py
@@ -2,6 +2,8 @@ from discord.ext.commands import check, Context
 
 def noadmin():
     async def predicate(ctx: Context):
+        if ctx.bot.ENV == "dev":
+            return True
         if ctx.me.guild_permissions.administrator:
             print(ctx.me.guild_permissions)
             await ctx.reply("I can't execute commands while I have the administrator permission.")
